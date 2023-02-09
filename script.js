a8b9d66c354fafca5089d4a6c2ed2abd336146ea
James Mitrani <jamesmitrani@gmail.com>
2023-02-08 19:35:54 -0500

    add function playRound

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice() {
   let n = Math.floor(Math.random() * 3);
   let computerSelection;
@@ -11,20 +12,51 @@ function getComputerChoice() {
   } else {
     console.log("Error"); 
   }
-  // returns the computer choice
-  console.log(computerSelection); 
+
+  computerSelection = computerSelection.toUpperCase();
+  // returns the computer choices
   return computerSelection;
 }
 
 function getPlayerChoice() {
   // asks for and returns the players choice
   let playerSelection = prompt("Please enter Rock, Paper, or Scissor");
+
+  playerSelection = playerSelection.toUpperCase();
   return playerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
+  let result;
+  
+  if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+    result = 0;
+    console.log("You Lose! Paper beats Rock");
+  } else if (playerSelection === "ROCK" && computerSelection === "SCISSOR") {
+    result = 1;
+    console.log("You Win! Rock beats Scissor");
+  } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+    result = 1;
+    console.log("You Win! Paper beats Rock");
+  } else if (playerSelection === "PAPER" && computerSelection === "SCISSOR") {
+    result = 0;
+    console.log("You Lose! Scissor beats Paper");
+  } else if (playerSelection === "SCISSOR" && computerSelection === "PAPER") {
+    result = 1;
+    console.log("You Win! Scissor beats Paper");
+  } else if (playerSelection === "SCISSOR" && computerSelection === "ROCK") {
+    result = 0;
+    console.log("You Lose! Rock beats Scissor");
+  //} else {
+    //result = "Tie"
+  }
 
+  return result;
 }
 
-getComputerChoice();
-getPlayerChoice();
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+console.log(computerSelection);
+playRound(playerSelection, computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
